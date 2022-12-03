9331bdc6d9e8472ef929b256deff81275fd96c76
Tunahan Danış <tunahandanis@outlook.com>
2022-12-03 10:52:10 +0300

    finish mockup single collection page

--- src/pages/collections/[collectionId].tsx
+++ src/pages/collections/[collectionId].tsx
@@ -1,8 +1,105 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"
+import { Row, Col } from "antd"
+import Link from "next/link"
+
+import styles from "../../styles/path-styles/Collections.module.scss"
+
+const nftInfos = [
+  {
+    name: "Random nft",
+    description: " Lorem ipsum dolor sit",
+    price: 5,
+    id: 23561,
+    imageUrl:
+      "https://images.wallpaperscraft.com/image/single/air_balloon_aerostat_art_128614_1920x1080.jpg",
+  },
+
+  {
+    name: "Another random nft",
+    description: " Lorem ipsum dolor sit",
+    price: 3,
+    id: 53251,
+    imageUrl:
+      "https://images.wallpaperscraft.com/image/single/air_balloon_aerostat_art_128614_1920x1080.jpg",
+  },
+
+  {
+    name: "Another random nft",
+    description: " Lorem ipsum dolor sit",
+    price: 10,
+    id: 64312,
+    imageUrl:
+      "https://images.wallpaperscraft.com/image/single/air_balloon_aerostat_art_128614_1920x1080.jpg",
+  },
+
+  {
+    name: "Random nft",
+    description: " Lorem ipsum dolor sit",
+    price: 150,
+    id: 64314,
+    imageUrl:
+      "https://images.wallpaperscraft.com/image/single/air_balloon_aerostat_art_128614_1920x1080.jpg",
+  },
+
+  {
+    name: "Random nft",
+    description: " Lorem ipsum dolor sit",
+    price: 67,
+    id: 35643,
+    imageUrl:
+      "https://images.wallpaperscraft.com/image/single/air_balloon_aerostat_art_128614_1920x1080.jpg",
+  },
+
+  {
+    name: "Random nft",
+    description: " Lorem ipsum dolor sit",
+    price: 1,
+    id: 75424,
+    imageUrl:
+      "https://images.wallpaperscraft.com/image/single/air_balloon_aerostat_art_128614_1920x1080.jpg",
+  },
+]
 
 const Collection = () => {
-  const router = useRouter();
-  return <div>{router.query.collectionId}</div>;
-};
+  const router = useRouter()
+  return (
+    <div>
+      <h2 className={styles.collectionTitle}>
+        Collection: {router.query.collectionId}
+      </h2>
+      <section className={styles.collectionGrid}>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          {nftInfos.map((nft) => (
+            <Col span={6} key={nft.id}>
+              <Link href="/" className={styles.collectionLink}>
+                <article className={styles.collectionCard}>
+                  <img src={nft.imageUrl} alt="nft image" />
+                  <div className={styles.collectionCardTextContainer}>
+                    <div className={styles.collectionCardInfo}>
+                      <p className={styles.collectionCardTitle}>{nft.name}</p>
+                      <p className={styles.collectionCardPrice}>Price</p>
+                    </div>
+                    <div className={styles.collectionCardInfoBottom}>
+                      <p className={styles.collectionCardDescription}>
+                        {nft.description}
+                      </p>
+                      <div className={styles.collectionCardPriceValue}>
+                        <img
+                          src="https://changenow.io/images/cached/xrp.png"
+                          alt="xrp logo"
+                        />{" "}
+                        <span>{nft.price}</span>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </section>
+    </div>
+  )
+}
 
-export default Collection;
+export default Collection
--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -1,10 +1,10 @@
-import { Row, Col } from "antd";
-import styles from "../../styles/path-styles/Collections.module.scss";
-import Link from "next/link";
+import { Row, Col } from "antd"
+import Link from "next/link"
+import styles from "../../styles/path-styles/Collections.module.scss"
 
 const testIds = [
   42153513, 35125312, 258225567, 26901285, 12570493, 24697043, 98172063,
-];
+]
 
 const Collections = () => {
   return (
@@ -14,7 +14,10 @@ const Collections = () => {
         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
           {testIds.map((id) => (
             <Col span={6} key={id}>
-              <Link href={`/collections/${id}`}>
+              <Link
+                href={`/collections/${id}`}
+                className={styles.collectionsLink}
+              >
                 <article className={styles.collectionsCard}>
                   <img
                     src="https://www.wallpaperflare.com/static/266/348/28/digital-art-pattern-vertical-portrait-display-wallpaper-preview.jpg"
@@ -33,7 +36,7 @@ const Collections = () => {
         </Row>
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default Collections;
+export default Collections
