b311b44a29181d6deeef404142df2cf2e9610c5c
Tunahan Danış <tunahandanis@outlook.com>
2022-12-07 02:46:54 +0300

    add metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,31 @@
-import "../styles/globals.scss";
-import type { AppProps } from "next/app";
-import "antd/dist/antd.css";
-import Header from "components/Header";
-import { AccountContextProvider } from "contexts/accountContext";
+import "../styles/globals.scss"
+import type { AppProps } from "next/app"
+import "antd/dist/antd.css"
+import Head from "next/head"
+import Header from "components/Header"
+import { AccountContextProvider } from "contexts/accountContext"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AccountContextProvider>
+      <Head>
+        <title>AiArtNFT | Generate artwork through AI, secure it as NFT</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/logo.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/logo.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/logo.png"
+        />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </AccountContextProvider>
-  );
+  )
 }
