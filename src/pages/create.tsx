0130db92e0cced414aa4c63574ae72a6d5611582
Tunahan Danış <tunahandanis@outlook.com>
2022-12-11 21:31:51 +0300

    change nft mint option order

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -5,7 +5,7 @@ import { useAccountContext } from "contexts/accountContext"
 import styles from "../styles/path-styles/Create.module.scss"
 
 const Create = () => {
-  const [selected, setSelected] = useState<string>("Upload")
+  const [selected, setSelected] = useState<string>("Generate")
   const [accountState] = useAccountContext()
 
   const mintNft = (imageUrl: string, name: string, description: string) => {
@@ -22,20 +22,20 @@ const Create = () => {
     <div className={styles.create}>
       <div className={styles.createTypeButtonContainer}>
         <button
-          onClick={() => setSelected("Upload")}
-          className={`${styles.createTypeButton} ${
-            styles.createTypeButtonUpload
-          } ${selected === "Upload" && styles.createTypeButtonSelected}`}
-        >
-          Upload
-        </button>
-        <button
           onClick={() => setSelected("Generate")}
           className={`${styles.createTypeButton} ${
             styles.createTypeButtonGenerate
           } ${selected === "Generate" && styles.createTypeButtonSelected}`}
         >
           Generate
+        </button>
+        <button
+          onClick={() => setSelected("Upload")}
+          className={`${styles.createTypeButton} ${
+            styles.createTypeButtonUpload
+          } ${selected === "Upload" && styles.createTypeButtonSelected}`}
+        >
+          Upload
         </button>
       </div>
       {selected === "Upload" ? (
