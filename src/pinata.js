4eda570859fa0dd0bb7f787c8324b2df36b055c8
Brian Lemba <72974932+Yakumwamba@users.noreply.github.com>
2022-12-12 10:00:43 +0200

    Update pinata.js

--- src/pinata.js
+++ src/pinata.js
@@ -3,7 +3,7 @@
 import axios from "axios"
 import FormData from "form-data"
 const { Readable } = require("stream")
-const JWT = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2MDdkMDI4Mi1hODMxLTRmNDctOWE1MC04OTk5ZjA5MjA0ZjAiLCJlbWFpbCI6ImJyaWFubGVtYmEyQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI0YmE2NDk1YTk2MTY0YTAwNTE1NiIsInNjb3BlZEtleVNlY3JldCI6IjVhY2I3Yjc3MjUxYzUzZDBkNjAyOGRiZGI4ODRkZjYyMWQzMTY2NzExZGY2ZTZjYjFjNTYwOWJiNjdlYjA4M2EiLCJpYXQiOjE2NzAzNzcxMTV9.3NGnpx8uNBGSVkdkIbmTc5-p_loMTvYiBIIuoOgr3DY`
+const JWT = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI1MDBjZDBlMi00NzA2LTQwYzMtYThkNy0wNDYyYTUwMjhiNTAiLCJlbWFpbCI6ImF0aGVuYW9pa29zQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI4NGIyYmFiMTk4M2Y4NjVjNjQ0ZSIsInNjb3BlZEtleVNlY3JldCI6IjM4MjBmN2E1NWJkMTgxOGM4M2EzMDVmNzJiZDZhMjQ4ZjM3NTZhYWQyZTI5MDJiMGMxM2RhZDNhOGNkOTRjODYiLCJpYXQiOjE2NzA4MzE5MDB9._MVXniScfQJV_fvxweO2cLoq3nGHHlc9FFc8ZTa-BFo`
 
 export const uploadJSONToIPFS = async (JSONBody) => {
   const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`
@@ -73,9 +73,9 @@ export const uploadFileToIPFS = async (file /* nft_name, description */) => {
       maxBodyLength: "Infinity",
       headers: {
         "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
-        pinata_api_key: "98b227205a75e86ced70",
+        pinata_api_key: "84b2bab1983f865c644e",
         pinata_secret_api_key:
-          "4dbbf292047abcb8584c913873d8fbabed457b5d5d753144541f2b0c71687fae",
+          "3820f7a55bd1818c83a305f72bd6a248f3756aad2e2902b0c13dad3a8cd94c86",
         Authorization: JWT,
       },
     })
