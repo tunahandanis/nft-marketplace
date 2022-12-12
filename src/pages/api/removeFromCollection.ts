e0c36672b8a7b179fd4344a4a01f695ef1cd415a
Yakumwamba <brianlemba@gmail.com>
2022-12-12 15:54:43 +0200

    added removeFromCollection endpoint

--- src/pages/api/removeFromCollection.ts
+++ src/pages/api/removeFromCollection.ts
@@ -3,17 +3,14 @@ import type { NextApiRequest, NextApiResponse } from "next"
 // import { getNFTMetadata, uploadFileToIPFS, uploadFromBuffer } from "pinata";
 
 import dbConnect from "../../../utils/dbConnect"
-//const Collection = require("../../models/collectionModel")
-const NFT = require("../../models/nftModel")
+const Collection = require("../../models/collectionModel")
+
 dbConnect()
-type NFTMetadata = {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  data: {}
-}
+
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<NFTMetadata>
+  res: NextApiResponse<any>
 ) {
 
     const collectionName = req.body.collectionName
